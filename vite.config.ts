a11574db0abdb277ebe2b3f15c826638ef257d3b
MOHAMMEDABRHO <kmaza012@gmail.com>
2025-07-19 03:34:07 +0100

    ecotrack

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/ecotrckr/' : '/', // <-- CRITICAL: Use /ecotrckr/
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+}));
